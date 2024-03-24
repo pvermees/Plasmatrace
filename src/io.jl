b8b956ce80e6b3149fd926d1ba238c3c8857971f
Jarred C Lloyd <jarredclloyd@gmail.com>
2024-03-24 20:53:00 +1030

    improve file reading performance, verbose names

--- src/io.jl
+++ src/io.jl
@@ -1,73 +1,82 @@
-function readFile(fname::AbstractString;instrument="Agilent")
-    if instrument=="Agilent"
-        sname, datetime, dat = readAgilent(fname)
-        bwin = autoWindow(dat[:,2:end],blank=true)
-        swin = autoWindow(dat[:,2:end],blank=false)
+function read_file(file::AbstractString; instrument::AbstractString = "Agilent")
+    if instrument == "Agilent"
+        sample_name, analysis_name, date_time, data = read_agilent(file)
+        blank_window = autoWindow(data[:, 2:end]; blank = true)
+        signal_window = autoWindow(data[:, 2:end]; blank = false)
     else
         PTerror("unknownInstrument")
     end
-    Sample(sname,datetime,dat,bwin,swin,"sample")
+    return Sample(sample_name, analysis_name, date_time, data, blank_window, signal_window, "sample")
 end
 
-function load(dname::AbstractString;instrument="Agilent")
-    fnames = readdir(dname)
-    samples = Vector{Sample}(undef,0)
-    datetimes = Vector{DateTime}(undef,0)
-    ext = getExt(instrument)
-    for fname in fnames
-        if occursin(ext,fname)
-            try
-                pname = joinpath(dname,fname)
-                samp = readFile(pname,instrument=instrument)
-                push!(samples,samp)
-                push!(datetimes,samp.datetime)
-            catch e
-                println("Failed to read "*fname)
-            end
+function load(directory_name::AbstractString; instrument::AbstractString = "Agilent")
+    files = glob(get_file_extension(instrument), directory_name)
+    analyses = Vector{Sample}(undef, length(files))
+    date_times = Vector{DateTime}(undef, length(files))
+    @threads for i in eachindex(files)
+        try
+            analyses[i] = read_file(files[i], instrument = instrument)
+            date_times[i] = analyses[i].date_time
+        catch
+            println("Failed to read " * files[i])
         end
     end
-    order = sortperm(datetimes)
-    sortedsamples = samples[order]
-    sorteddatetimes = datetimes[order]
-    dt = sorteddatetimes .- sorteddatetimes[1]
-    runtime = Dates.value.(dt)./sph
-    for i in eachindex(sortedsamples)
-        samp = sortedsamples[i]
-        samp.dat[:,1] = samp.dat[:,2]./sph .+ runtime[i]
+    order = sortperm(date_times)
+    sorted_analyses = analyses[order]
+    sorted_date_times = date_times[order]
+    dt = sorted_date_times .- sorted_date_times[1]
+    runtime = Dates.value.(dt) ./ hour_seconds
+    for i in eachindex(sorted_analyses)
+        analysis = sorted_analyses[i]
+        analysis.data[:, 1] = analysis.data[:, 2] ./ hour_seconds .+ runtime[i]
     end
-    return sortedsamples
+    return sorted_analyses
 end
 export load
 
-function readAgilent(fname::AbstractString,
-    date_time_format::DateFormat=Dates.DateFormat("d/m/Y H:M:S"))
-    f = open(fname, "r")
-    strs = readlines(f)
+function read_agilent(
+    file::AbstractString,
+    date_time_format::DateFormat = Dates.DateFormat("d/m/Y H:M:S"),
+)
+    f = open(file, "r")
+    lines = readlines(f)
     # read header
-    sname = split.(split(strs[1],"\\"),"/")[end][end]
-    datetime = strs[3][findfirst(":", strs[3])[1]+2:findlast(":", strs[3])[1]+2]
-    datetime = Dates.DateTime(datetime, date_time_format)
-    if Dates.Year(datetime) < Dates.Year(2000)
-        datetime += Dates.Year(2000)
-    else
+    analysis_name = split(lines[1], "\\")[end][begin:(end - 2)]
+    sample_name = rstrip(analysis_name[begin:(findlast("-", analysis_name)[1] - 1)])
+    date_time = lines[3][(findfirst(":", lines[3])[1] + 2):(findlast(":", lines[3])[1] + 2)]
+    date_time = Dates.DateTime(date_time, date_time_format)
+    if Dates.Year(date_time) < Dates.Year(2000)
+        date_time += Dates.Year(2000)
     end
-    labels = split(strs[4],",")
+    data = CSV.read(
+        file,
+        DataFrame;
+        header = 4,
+        skipto = 5,
+        footerskip = 3,
+        ignoreemptyrows = true,
+        normalizenames = true,
+        delim = ',',
+    )
+    rename!(data, "Time_Sec_" => "run_time")
 
-    # read signals
-    nr = size(strs,1)
-    measurements = mapreduce(vcat, strs[5:(nr-3)]) do s
-        (parse.(Float64, split(s, ",")))'
-    end
-    labels = ["Run Time [hours]";labels]
-    dat = DataFrame(hcat(measurements[:,1]./sph,measurements),labels)
+    # @time labels = split(lines[4], ",")
+
+    # # read signals
+    # @time nr = size(lines, 1)
+    # @time measurements = mapreduce(vcat, lines[5:(nr - 3)]) do s
+    #     return (parse.(Float64, split(s, ",")))'
+    # end
+    # @time labels = ["Run Time [hours]"; labels]
+    # @time data = DataFrame(hcat(measurements[:, 1] ./ hour_seconds, measurements), labels)
 
     close(f)
-    return sname, datetime, dat
+    return sample_name, analysis_name, date_time, data
 end
 
-function getExt(instrument)
+function get_file_extension(instrument)
     if instrument == "Agilent"
-        return ".csv"
+        return "*.csv"
     else
         PTerror("unknownInstrument")
     end
