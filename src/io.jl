28aa0d914ded935314e84e887312e2981fa8d5dc
Jarred C Lloyd <jarredclloyd@gmail.com>
2024-03-25 16:25:21 +1030

    performance optimisations, retain local time, add run time as new col

--- src/io.jl
+++ src/io.jl
@@ -6,7 +6,15 @@ function read_file(file::AbstractString; instrument::AbstractString = "Agilent")
     else
         PTerror("unknownInstrument")
     end
-    return Sample(sample_name, analysis_name, date_time, data, blank_window, signal_window, "sample")
+    return Sample(
+        sample_name,
+        analysis_name,
+        date_time,
+        data,
+        blank_window,
+        signal_window,
+        "sample",
+    )
 end
 
 function load(directory_name::AbstractString; instrument::AbstractString = "Agilent")
@@ -15,22 +23,19 @@ function load(directory_name::AbstractString; instrument::AbstractString = "Agil
     date_times = Vector{DateTime}(undef, length(files))
     @threads for i in eachindex(files)
         try
-            analyses[i] = read_file(files[i], instrument = instrument)
+            analyses[i] = read_file(files[i]; instrument = instrument)
             date_times[i] = analyses[i].date_time
         catch
             println("Failed to read " * files[i])
         end
     end
     order = sortperm(date_times)
-    sorted_analyses = analyses[order]
-    sorted_date_times = date_times[order]
-    dt = sorted_date_times .- sorted_date_times[1]
-    runtime = Dates.value.(dt) ./ hour_seconds
-    for i in eachindex(sorted_analyses)
-        analysis = sorted_analyses[i]
-        analysis.data[:, 1] = analysis.data[:, 2] ./ hour_seconds .+ runtime[i]
+    analyses = analyses[order]
+    @threads for i in eachindex(analyses)
+        analyses[i].data.run_time_hours .+=
+            (Dates.value(analyses[i].date_time - date_times[begin]) / hour_milliseconds)
     end
-    return sorted_analyses
+    return analyses
 end
 export load
 
@@ -38,16 +43,18 @@ function read_agilent(
     file::AbstractString,
     date_time_format::DateFormat = Dates.DateFormat("d/m/Y H:M:S"),
 )
-    f = open(file, "r")
-    lines = readlines(f)
+    lines = split(readuntil(file, "Time "), "\n")
     # read header
     analysis_name = split(lines[1], "\\")[end][begin:(end - 2)]
     sample_name = rstrip(analysis_name[begin:(findlast("-", analysis_name)[1] - 1)])
-    date_time = lines[3][(findfirst(":", lines[3])[1] + 2):(findlast(":", lines[3])[1] + 2)]
+    date_time = rstrip(
+        lines[3][(findfirst(":", lines[3])[1] + 2):(findfirst("using", lines[3])[1] - 1)],
+    )
     date_time = Dates.DateTime(date_time, date_time_format)
     if Dates.Year(date_time) < Dates.Year(2000)
         date_time += Dates.Year(2000)
     end
+    #read data
     data = CSV.read(
         file,
         DataFrame;
@@ -58,19 +65,9 @@ function read_agilent(
         normalizenames = true,
         delim = ',',
     )
-    rename!(data, "Time_Sec_" => "run_time")
+    rename!(data, "Time_Sec_" => "local_time_seconds")
+    insertcols!(data, 2, "run_time_hours" => data.local_time_seconds ./ 3600)
 
-    # @time labels = split(lines[4], ",")
-
-    # # read signals
-    # @time nr = size(lines, 1)
-    # @time measurements = mapreduce(vcat, lines[5:(nr - 3)]) do s
-    #     return (parse.(Float64, split(s, ",")))'
-    # end
-    # @time labels = ["Run Time [hours]"; labels]
-    # @time data = DataFrame(hcat(measurements[:, 1] ./ hour_seconds, measurements), labels)
-
-    close(f)
     return sample_name, analysis_name, date_time, data
 end
 
